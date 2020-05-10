82ccde78b7bcb666973fdee59b3b81c15e07772a
Ashley-Gail Wright <ashley.gail.wright@gmail.com>
2020-05-09 18:14:38 -0700

    Refactored Inline Styling to use StyleSheet

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { TextInput, View, Button, Text } from 'react-native';
+import { TextInput, View, Button, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -15,25 +15,52 @@ export default function App() {
   }
 
 
+
   return (
-    <View style={{padding: 50}}>
+    <View style={styles.mainView}>
       {/* flexDirection's default value is column
         For more information: https://reactnative.dev/docs/flexbox */}
-      <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
+      {/* Using stylesheets are better than using inline styling
+       <View style={{flexDirection: 'row',justifyContent: 'space-between'}}> */}
+      <View style={styles.inputContainer}>
         <TextInput
          placeholder="Course Goal"
          onChangeText={goalInputHandler}
-         style={{borderColor: 'black', 
-                borderWidth: 1, 
-                padding: 10,
-                width: '80%'}}/>
+         style={styles.input}/>
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-
+      
       <View>
-        {courseGoals.map((goal) => <Text>{goal}</Text>)}
+        {courseGoals.map((goal) =>
+          <View style={styles.listItem}><Text>{goal}</Text></View>)}
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    padding: 50
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  },
+
+  listItem: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#fee9e1',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
